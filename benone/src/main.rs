c761db9b53122731902c877f9d67992527c04942
Ben Ashford <benashford@users.noreply.github.com>
2018-01-13 21:28:50 +0000

    Initial bot that doesn't do very much

--- benone/src/main.rs
+++ benone/src/main.rs
@@ -1,13 +1,54 @@
 extern crate battlecode_engine as bc;
+extern crate failure;
+extern crate fnv;
 extern crate time;
+extern crate rand;
 
 mod turn;
 
 use bc::controller::GameController;
+use bc::location::{Direction, Location};
+
+use failure::Error;
 
 use turn::Turn;
 
-fn do_turn(gc: &mut GameController, turn: &Turn) {}
+fn do_workers(gc: &mut GameController, turn: &mut Turn) -> Result<(), Error> {
+    for &worker_id in turn.my_units.workers.iter() {
+        let rand_direction = **rand::seq::sample_iter(&mut turn.rng, &turn.directions, 1).unwrap().get(0).unwrap();
+        if gc.can_replicate(worker_id, rand_direction) {
+            gc.replicate(worker_id, rand_direction)?;
+            continue;
+        }
+        let location = match gc.unit_ref(worker_id)?.location() {
+            Location::OnMap(location) => location,
+            _ => continue
+        };
+        // TODO - replace with "find nearest karbonite"
+        let known_karbonite = turn.known_karbonite[location.y as usize][location.x as usize];
+        if known_karbonite > 0 {
+            let actual_karbonite = gc.karbonite_at(location)?;
+            turn.known_karbonite[location.y as usize][location.x as usize] = actual_karbonite;
+            if actual_karbonite > 0 && gc.can_harvest(worker_id, Direction::Center) {
+                gc.harvest(worker_id, Direction::Center)?;
+                continue;
+            }
+        } else {
+            if gc.is_move_ready(worker_id) {
+                if gc.can_move(worker_id, rand_direction) {
+                    gc.move_robot(worker_id, rand_direction)?;
+                }
+            }
+        }
+    }
+    Ok(())
+}
+
+fn do_turn(gc: &mut GameController, turn: &mut Turn) -> Result<(), Error> {
+    do_workers(gc, turn)?;
+
+    Ok(())
+}
 
 fn main() {
     println!("Starting Benone");
@@ -16,15 +57,18 @@ fn main() {
     let mut turn = Turn::new(&gc);
 
     loop {
+        let round = gc.round();
+        let karbonite = gc.karbonite();
+        println!("START OF TURN {}, karbonite: {}", round, karbonite);
         turn.update(&gc);
-        do_turn(&mut gc, &turn);
+        do_turn(&mut gc, &mut turn).expect("Turn failed");
         let turn_end = time::precise_time_ns();
         println!(
-            "END OF TURN {}, took: {:.3}ns",
-            gc.round(),
-            turn_end - turn_start
+            "END OF TURN {}, took: {:.3}ms",
+            round,
+            (turn_end - turn_start) as f64 / 1000000.0f64
         );
-        gc.next_turn();
+        gc.next_turn().expect("Cannot start next turn");
         turn_start = time::precise_time_ns();
     }
 
--- benone/src/turn.rs
+++ benone/src/turn.rs
@@ -1,11 +1,86 @@
+use rand;
+
 use bc::controller::GameController;
+use bc::location::Direction;
+use bc::unit::{Unit, UnitID, UnitType};
+use bc::world::Team;
 
-pub(crate) struct Turn {}
+#[derive(Debug, Default)]
+pub(crate) struct KnownUnits {
+    pub(crate) workers: Vec<UnitID>,
+    pub(crate) knights: Vec<UnitID>,
+    pub(crate) rangers: Vec<UnitID>,
+    pub(crate) mages: Vec<UnitID>,
+    pub(crate) healers: Vec<UnitID>,
+    pub(crate) factories: Vec<UnitID>,
+    pub(crate) rockets: Vec<UnitID>,
+}
 
-impl Turn {
-    pub(crate) fn new(gc: &GameController) -> Turn {
-        Turn {}
+impl KnownUnits {
+    fn reset(&mut self) {
+        self.workers.clear();
+        self.knights.clear();
+        self.rangers.clear();
+        self.mages.clear();
+        self.healers.clear();
+        self.factories.clear();
+        self.rockets.clear();
     }
 
-    pub(crate) fn update(&mut self, gc: &GameController) {}
+    fn add(&mut self, unit: &Unit) {
+        let unit_id = unit.id();
+        match unit.unit_type() {
+            UnitType::Worker => self.workers.push(unit_id),
+            UnitType::Knight => self.knights.push(unit_id),
+            UnitType::Ranger => self.rangers.push(unit_id),
+            UnitType::Mage => self.mages.push(unit_id),
+            UnitType::Healer => self.healers.push(unit_id),
+            UnitType::Factory => self.factories.push(unit_id),
+            UnitType::Rocket => self.factories.push(unit_id),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub(crate) struct Turn {
+    pub(crate) rng: rand::ThreadRng,
+
+    pub(crate) directions: Vec<Direction>,
+
+    pub(crate) known_karbonite: Vec<Vec<u32>>,
+
+    pub(crate) my_units: KnownUnits,
+    pub(crate) enemy_units: KnownUnits,
+}
+
+impl Turn {
+    pub(crate) fn new(gc: &GameController) -> Self {
+        let planet = gc.planet();
+        let starting_map = gc.starting_map(planet);
+
+        let mut turn = Turn {
+            rng: rand::thread_rng(),
+            directions: Direction::all(),
+            known_karbonite: starting_map.initial_karbonite.clone(),
+            my_units: Default::default(),
+            enemy_units: Default::default(),
+        };
+        turn.update(gc);
+        turn
+    }
+
+    pub(crate) fn update(&mut self, gc: &GameController) {
+        let my_team = gc.team();
+
+        self.my_units.reset();
+        self.enemy_units.reset();
+
+        for unit_ref in gc.units_ref() {
+            if unit_ref.team() == my_team {
+                self.my_units.add(unit_ref);
+            } else {
+                self.enemy_units.add(unit_ref);
+            }
+        }
+    }
 }
